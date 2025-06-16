02e9d51081104b69e4803afa3ca860be92a03304
Gabriel <gabrielbellliti@gmail.com>
2025-06-16 14:26:50 +0200

    .

---
+++ nouveau_modele/simu.py
@@ -0,0 +1,97 @@
+import numpy as np
+from classement import SystemeElo, SystemeGlicko, SystemeGlicko2
+import math
+
+K = 32
+
+class Joueur:
+    _id_counter = 0
+
+    def __init__(self, force=None):
+        self.id = Joueur._id_counter
+        Joueur._id_counter += 1
+        self.force = force if force is not None else self.generer_force()
+        self.historique_scores = []
+
+        self.elo_simu = SystemeElo()
+        self.elo_estime = SystemeElo()
+        self.glicko_simu = SystemeGlicko()
+        self.glicko_estime = SystemeGlicko()
+        self.glicko2_simu = SystemeGlicko2()
+        self.glicko2_estime = SystemeGlicko2()
+
+    @staticmethod
+    def generer_force():
+        x = np.random.lognormal(mean=-0.5, sigma=0.5)
+        return 1 / (1 + np.exp(-np.log(x)))
+
+    def enregistrer_score(self, score):
+        self.historique_scores.append(score)
+
+
+def sigmoid(x, k=10):
+    return 1 / (1 + np.exp(-k * x))
+
+def modifie_hasard(taux, impact_force_hasard, fmax):
+    eps = 1e-8
+    denom = 1 / np.exp(-impact_force_hasard + eps) - 0.5
+    denom = np.clip(denom, eps, None)
+    a = (taux - 1) / denom
+    b = 1 - a / 2
+    result = a / (1 + np.exp(-impact_force_hasard * fmax)) + b
+    return np.clip(result, 0, 1)
+
+def get_proba_simu(f1, f2, taux, impact):
+    diff = abs(f1 - f2)
+    mf = max(f1, f2)
+    k_hasard = -np.log(modifie_hasard(taux, impact, mf))
+    return sigmoid(diff, k=k_hasard * 300 * diff)
+
+def tirage_bernoulli(p):
+    return np.random.binomial(1, p)
+
+def rencontre_simu(f1, f2, jeu):
+    p = get_proba_simu(f1, f2, jeu.taux_de_hasard, jeu.impact_force_hasard)
+    gagnant_est_plus_fort = tirage_bernoulli(p)
+    if f1 > f2:
+        return (1, 0) if gagnant_est_plus_fort else (0, 1)
+    else:
+        return (0, 1) if gagnant_est_plus_fort else (1, 0)
+
+def mettre_a_jour_elo(j1, j2, S1, S2, mode):
+    if mode == "simu":
+        j1.elo_simu.update(j2.elo_simu.rating, S1)
+        j2.elo_simu.update(j1.elo_simu.rating, S2)
+    else:
+        j1.elo_estime.update(j2.elo_estime.rating, S1)
+        j2.elo_estime.update(j1.elo_estime.rating, S2)
+    j1.enregistrer_score(S1)
+    j2.enregistrer_score(S2)
+
+def mettre_a_jour_glicko(j1, j2, S1, S2, mode):
+    if mode == "simu":
+        r2, rd2 = j2.glicko_simu.get_rating()
+        r1, rd1 = j1.glicko_simu.get_rating()
+        j1.glicko_simu.update([(r2, rd2)], [S1])
+        j2.glicko_simu.update([(r1, rd1)], [S2])
+    else:
+        r2, rd2 = j2.glicko_estime.get_rating()
+        r1, rd1 = j1.glicko_estime.get_rating()
+        j1.glicko_estime.update([(r2, rd2)], [S1])
+        j2.glicko_estime.update([(r1, rd1)], [S2])
+    j1.enregistrer_score(S1)
+    j2.enregistrer_score(S2)
+
+def mettre_a_jour_glicko2(j1, j2, S1, S2, mode):
+    if mode == "simu":
+        r2 = j2.glicko2_simu.get_rating()
+        r1 = j1.glicko2_simu.get_rating()
+        j1.glicko2_simu.update([r2], [S1])
+        j2.glicko2_simu.update([r1], [S2])
+    else:
+        r2 = j2.glicko2_estime.get_rating()
+        r1 = j1.glicko2_estime.get_rating()
+        j1.glicko2_estime.update([r2], [S1])
+        j2.glicko2_estime.update([r1], [S2])
+    j1.enregistrer_score(S1)
+    j2.enregistrer_score(S2)
